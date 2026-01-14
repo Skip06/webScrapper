2bc2fafefd7d742e1c19c611a3492e902f3e7e45
Swastik <swastik.patro02@gmail.com>
2026-01-14 17:01:12 +0530

    added parsing and scraping logic

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,1 +1,16 @@
-# the .css() here will return the list of all 
+from selectolax.parser import HTMLParser
+from models.datamodels import Quotes
+
+def parseQuotes(html: str):
+    tree = HTMLParser(html)
+    quotes = []
+    for node in tree.css('div.quote'):  # returns all <div class="quote">
+        text = node.css_first('span.text').text()
+        author = node.css_first('small.author').text()
+        tags = [tag.text() for tag in node.css('div.tags a.tag')]
+
+        quoteObj = Quotes(text=text, author=author, tags=tags)
+        print(quoteObj)
+        quotes.append(quoteObj)
+
+    return quotes
